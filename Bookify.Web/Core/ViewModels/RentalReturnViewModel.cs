b94001fcc63034e347080d7ffe928342ce28bc83
youssef salah <140291980+youssef-salah1@users.noreply.github.com>
2025-08-28 12:42:00 +0300

    Edit

---
+++ Bookify.Web/Core/ViewModels/RentalReturnViewModel.cs
@@ -0,0 +1,23 @@
+﻿using UoN.ExpressiveAnnotations.NetCore.Attributes;
+
+namespace Bookify.Web.Core.ViewModels;
+
+public class RentalReturnViewModel
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Penalty Paid?")]
+    [AssertThat("(TotalDelayInDays == 0 && PenaltyPaid == false) || PenaltyPaid == true", ErrorMessage = "Penalty must be paid if there is a delay.")]
+    public bool PenaltyPaid { get; set; }
+
+    public IList<RentalCopyViewModel> Copies { get; set; } = new List<RentalCopyViewModel>();
+
+    public List<ReturnCopyViewModel> SelectedCopies { get; set; } = new List<ReturnCopyViewModel>();
+
+    public bool AllowExtend { get; set; }
+
+    public int TotalDelayInDays
+    {
+        get { return Copies.Sum(c => c.DelayInDays); }
+    }
+}
---
+++ Bookify.Web/wwwroot/js/subscriber-details.js
@@ -0,0 +1,89 @@
+﻿$(document).ready(function () {
+    $('.js-renew').on('click', function () {
+
+        var subscriberKey = $(this).data('key'); 
+
+        bootbox.confirm({
+            message: "Are you sure that you need to renew this subscription?",
+            buttons: {
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'No',
+                    className: 'btn-secondary'
+                }
+            },
+            callback: function (result) {
+                if (result) {
+                    $.post({
+                        url: `/Subscribers/RenewSubscription?sKey=${subscriberKey}`,
+                        data: {
+                            '__RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val()
+                        },
+                        success: function (row) {
+                            $('#SubscriptionsTable').find('tbody').append(row);
+
+                            var activeIcon = $('#ActiveStatusIcon');
+                            activeIcon.removeClass('d-none');
+                            activeIcon.siblings('svg').remove();
+                            activeIcon.parents('.card').removeClass('bg-warning').addClass('bg-success');
+                            $('#RantalButton').removeClass('d-none');
+                            $('#CardStatus').text('Active subscriber');
+                            $('#StatusBadge').removeClass('badge-light-warning').addClass('badge-light-success').text('Active subscriber');
+                            showSuccessMessage();
+                        },
+                        error: function () {
+                            showErrorMessage();
+                        }
+                    });
+                }
+            }
+        });
+    });
+    $('.js-cancel-rental').on('click', function () {
+
+        var btn = $(this);
+
+        bootbox.confirm({
+            message: "Are you sure that you need to cancel this rental?",
+            buttons: {
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-danger'
+                },
+                cancel: {
+                    label: 'No',
+                    className: 'btn-secondary'
+                }
+            },
+            callback: function (result) {
+                if (result) {
+                    $.post({
+                        url: `/Rentals/MarkAsDeleted/${btn.data('id')}`,
+                        data: {
+                            '__RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val()
+                        },
+                        success: function (result) {
+                            btn.parents('tr').remove();
+
+                            if ($('#RentalsTable tbody tr').length === 0) {
+                                $('#RentalsTable').fadeOut(function () {
+                                    $('#Alert').fadeIn();
+                                });
+                            }
+
+                            var count = $('#RentalsCount');
+                            var newCount = parseInt(count.text()) - result;
+                            count.text(newCount);
+                        },
+                        error: function () {
+                            showErrorMessage();
+                        }
+                    });
+                }
+            }
+        });
+    });
+});
